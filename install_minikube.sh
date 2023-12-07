abdee6215b406a27db1fc73981c1cb5d8af11ed8
JHJORTIZ <JHJORTIZ@bancolombia.com.co>
2023-12-07 10:01:33 -0500

    Provision scripts were reduced avoiding unnecessary decoupling

--- install_minikube.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "\nInstalling minikube\n"
-echo -e "This may take a while...\n"
-
-sudo apt update -y
-
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
