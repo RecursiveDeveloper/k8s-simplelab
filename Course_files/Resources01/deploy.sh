61a263a1e5c08252e51936eb91f0eb58cdaccec3
jhjortiz <jhjortiz@bancolombia.com.co>
2024-03-26 11:19:49 -0500

    Resources01 deployment script fixed for automatic creation when executing vagrant up command

--- Course_files/Resources01/deploy.sh
+++ Course_files/Resources01/deploy.sh
@@ -2,10 +2,18 @@
 
 namespace="app"
 configmap_name="nginx-config-dir"
-configmap_dir="nginx"
+configmap_dir="/opt/Course_files/Resources01/nginx"
+manifest_files="/opt/Course_files/Resources01"
 
 microk8s kubectl create namespace ${namespace}
 
-microk8s kubectl create configmap ${configmap_name} --from-file=${configmap_dir} -n ${namespace}
+microk8s kubectl create configmap ${configmap_name} \
+    --from-file=${configmap_dir} \
+    -n ${namespace}
 
-microk8s kubectl apply -f . -n ${namespace}
+microk8s kubectl apply \
+    -f $manifest_files/persistentVolume.yaml \
+    -f $manifest_files/persistentVolumeClaim.yaml \
+    -f $manifest_files/service.yaml \
+    -f $manifest_files/deployment.yaml \
+    -n ${namespace}
--- install_k8s.sh
+++ install_k8s.sh
@@ -30,31 +30,6 @@ sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
 #---------------------------------------------
-echo -e "\nInstalling Docker Engine\n"
-#---------------------------------------------
-# Add Docker's official GPG key:
-sudo apt-get -y update
-sudo apt-get -y install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get -y update
-
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd docker
-sudo usermod -aG docker $OWNER
-sudo newgrp docker
-
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-
-#---------------------------------------------
 echo -e "\nInstalling Kubectl\n"
 #---------------------------------------------
 sudo apt update -y
@@ -75,5 +50,6 @@ sudo snap install microk8s --classic
 microk8s status --wait-ready
 
 sudo usermod -a -G microk8s $OWNER
+sudo mkdir -p ~/.kube
 sudo chown -f -R $OWNER ~/.kube
 sudo newgrp microk8s
