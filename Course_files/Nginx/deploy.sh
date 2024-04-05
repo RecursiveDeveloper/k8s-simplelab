836b4da4d0aec262ddcf27f71d88b0f09467a6aa
Jhoan Jesus Ortiz Sandoval <johanjesus500@gmail.com>
2024-04-04 21:53:11 -0500

    Fixes implemented for deploy and destroy script. Secret object added to be mounted in deployment manifest

--- Course_files/Nginx/deploy.sh
+++ Course_files/Nginx/deploy.sh
@@ -2,13 +2,20 @@
 
 manifest_files="/opt/Course_files/Nginx"
 configmap_dir="${manifest_files}/config_files"
+secret_dir="${manifest_files}/secrets"
+
 namespace="app"
-configmap_name="nginx-config-dir"
+configmap_name="nginx-config"
+secret_name="nginx-secret"
 
 microk8s kubectl create namespace ${namespace}
 
 microk8s kubectl create configmap ${configmap_name} \
     --from-file=${configmap_dir} \
+    -n ${namespace}
+
+microk8s kubectl create secret generic ${secret_name} \
+    --from-file=${secret_dir} \
     -n ${namespace}
 
 microk8s kubectl apply \
--- Course_files/Nginx/destroy.sh
+++ Course_files/Nginx/destroy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 namespace="app"
+configmap_name="nginx-config"
+secret_name="nginx-secret"
 
 microk8s kubectl delete -f . -n ${namespace}
+microk8s kubectl delete configmap ${configmap_name} -n ${namespace}
+microk8s kubectl delete secret ${secret_name} -n ${namespace}
+microk8s kubectl delete namespace ${namespace}
