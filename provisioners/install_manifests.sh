90b4a24ff7b873d269a697a4e38de0e232bf9798
jhjortiz <jhjortiz@bancolombia.com.co>
2024-09-02 11:33:51 -0500

    basic exercise deploy uncommented

--- provisioners/install_manifests.sh
+++ provisioners/install_manifests.sh
@@ -9,8 +9,8 @@ echo -e "\nFormatting Scripts - dos2unix\n"
 apt install dos2unix -y
 find ${sync_folder} -type f -print0 | xargs -0 dos2unix
 
-# echo -e "\nDeploying Kubernetes Manifests\n"
-# bash ${basic_exercise_folder}/deploy.sh ${owner} ${basic_exercise_folder}
+echo -e "\nDeploying Kubernetes Manifests\n"
+bash ${basic_exercise_folder}/deploy.sh ${owner} ${basic_exercise_folder}
 
 echo -e "\nDeploying Kubernetes Ingress\n"
 bash ${ingress_exercise_folder}/deploy.sh ${owner} ${ingress_exercise_folder}
